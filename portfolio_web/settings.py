13696b04028addf9f8a4b70220611a0376bb9cd9
shubham-khulbe <skhulbe@dal.ca>
2019-06-15 21:09:29 -0300

    changed DB to postgres

--- portfolio_web/settings.py
+++ portfolio_web/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portfolio_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'Shubham@25',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
