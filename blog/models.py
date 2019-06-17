4e97a85c5186d0ca3a0048240e3513c4cbdb2b75
shubham-khulbe <skhulbe@dal.ca>
2019-06-17 10:03:02 -0300

    homepage added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='image/')
+    summary = models.TextField()
+    date_pub = models.DateTimeField()
