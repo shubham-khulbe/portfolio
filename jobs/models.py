6ddeeb99b82d659db8a15187f1b77345355c3ef1
shubham-khulbe <skhulbe@dal.ca>
2019-06-19 09:06:19 -0300

    added details to job cards

--- jobs/models.py
+++ jobs/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class Job(models.Model):
     image = models.ImageField(upload_to='image/')
-    summary = models.CharField(max_length=200)
+    summary = models.CharField(max_length=500)
